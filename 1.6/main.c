bd10313b3f098b792e45c0c662de67fa50602c19
ThiPresotto <145331013+ThiPresotto@users.noreply.github.com>
2025-09-29 11:38:46 -0300

    Added text to winners

--- 1.6/main.c
+++ 1.6/main.c
@@ -1,16 +1,26 @@
 #include <SDL2/SDL.h>
 #include <stdbool.h>
+#include <SDL2/SDL_ttf.h>
+#include <assert.h>
 
 
 int main (int argc, char* args[]){
 
     /*INICIALIZAÇÃO*/
     SDL_Init(SDL_INIT_EVERYTHING);
+    TTF_Init();
+
     SDL_Window* window = SDL_CreateWindow("3 Retângulos",
         SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,
         600,600,SDL_WINDOW_SHOWN
     );
     SDL_Renderer* ren = SDL_CreateRenderer(window,-1,0);
+    TTF_Font* fnt = TTF_OpenFont("tiny.ttf",20);
+    assert(fnt != NULL);
+    SDL_Color clr = {0xFF,0x00,0x00,0xFF};
+    SDL_Surface* sfc;
+    SDL_Texture* txt;
+    
 
 
     /*EXECUÇÃO*/
@@ -21,6 +31,7 @@ int main (int argc, char* args[]){
     int espera = 1000;
     int winner;
     bool fin[3] = {false,false,false};
+    SDL_Rect vencedor = { 250,500, 150,80 };
 
     while(1){
         SDL_SetRenderDrawColor(ren, 0xFF,0xFF,0xFF,0xFF);
@@ -48,10 +59,10 @@ int main (int argc, char* args[]){
                 if(fin[1]) continue;
                 switch(evt.key.keysym.sym){
                     case SDLK_UP:
-                    r2.x += 10;
+                    r2.x += 20;
                     break;
                     case SDLK_DOWN:
-                    r2.x -= 10;
+                    r2.x -= 20;
                     break;
                     case SDLK_LEFT:
                     r2.x -= 5;
@@ -101,16 +112,34 @@ int main (int argc, char* args[]){
                     r1 = (SDL_Rect) {270,270,60,60};
                     SDL_SetRenderDrawColor(ren,0xFF,0x00,0x00,0x00);
                     SDL_RenderFillRect(ren,&r1);   
+                    clr = (SDL_Color) {0xFF,0x00,0x00,0xFF};
+                    sfc = TTF_RenderText_Blended(fnt,"red",clr);
+                    assert(sfc != NULL);
+                    txt = SDL_CreateTextureFromSurface(ren, sfc);
+                    assert(txt != NULL);
+                    SDL_RenderCopy(ren,txt,NULL,&vencedor);
                     break;
                 case 1:
                     r2 = (SDL_Rect) {270,270,60,60};
                     SDL_SetRenderDrawColor(ren,0x00,0xFF,0x00,0x00);
-                    SDL_RenderFillRect(ren,&r2);   
+                    SDL_RenderFillRect(ren,&r2);  
+                    clr = (SDL_Color) {0x00,0xFF,0x00,0xFF};
+                    sfc = TTF_RenderText_Blended(fnt,"green",clr);
+                    assert(sfc != NULL);
+                    txt = SDL_CreateTextureFromSurface(ren, sfc);
+                    assert(txt != NULL);
+                    SDL_RenderCopy(ren,txt,NULL,&vencedor); 
                     break;
                 case 2:
                     r3 = (SDL_Rect) {270,270,60,60};
                     SDL_SetRenderDrawColor(ren,0x00,0x00,0xFF,0x00);
                     SDL_RenderFillRect(ren,&r3);   
+                    clr = (SDL_Color) {0x00,0x00,0xFF,0xFF};
+                    sfc = TTF_RenderText_Blended(fnt,"blue",clr);
+                    assert(sfc != NULL);
+                    txt = SDL_CreateTextureFromSurface(ren, sfc);
+                    assert(txt != NULL);
+                    SDL_RenderCopy(ren,txt,NULL,&vencedor);
                     break;
             }
             SDL_Delay(30);
@@ -126,14 +155,20 @@ int main (int argc, char* args[]){
                         break;
                     }
                 }
+                if(evt.type == SDL_QUIT){
+                    goto exit;
+                }
+                
             }
         }
 
 
     }
+    exit:
 
     /*FINALIZAÇÃO*/
     SDL_DestroyRenderer(ren);
+    SDL_FreeSurface(sfc);
     SDL_DestroyWindow(window);
     SDL_Quit();
 
